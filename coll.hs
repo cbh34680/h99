8085ab61776871a3f8b86f0ec2d31fd3ed403cd2
cbh34680 <cbh34680@gmail.com>
2023-01-24 11:29:18 +0900

    test

--- coll.hs
+++ coll.hs
@@ -1,4 +1,22 @@
 
--- takeTo :: [Int] -> [Int]
+gen = [3,7,9,8,5,4,2]
 
-takeTo xs = foldr (\x acc -> [100]) [] xs :: [Int]
+takeTo :: (a -> Bool) -> [a] -> [a]
+
+takeTo f xs = foldr (\x acc -> if f x then [x] else x:acc) [] xs
+
+{-
+
+takeTo even [3,7,9,8,5,4,2]
+
+	1. x=2 acc=[]       --[even 2/T]--> ret=[2]
+	2. x=4 acc=[2]      --[even 4/T]--> ret=[4]
+	3. x=5 acc=[4]      --[even 5/F]--> ret=[5,4]
+	3. x=8 acc=[5,4]    --[even 8/T]--> ret=[8]
+	3. x=9 acc=[8]      --[even 9/F]--> ret=[9,8]
+	3. x=7 acc=[9,8]    --[even 7/F]--> ret=[7,9,8]
+	3. x=3 acc=[7,9,8]  --[even 3/F]--> ret=[3,7,9,8]
+
+--> [3,7,9,8]
+
+-}
