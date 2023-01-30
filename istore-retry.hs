c5af0abd5783f99f87e7c6102f9534c191d67278
cbh34680 <cbh34680@iret.co.jp>
2023-01-30 23:58:38 +0900

    test

--- istore-retry.hs
+++ istore-retry.hs
@@ -1,23 +1,40 @@
 
 class IStore s where
   empty :: s
-  put :: Integer -> s -> s
-  get :: s -> Maybe (Integer, s)
-
---
-newtype IList = IList [Integer] deriving Show
-
-instance IStore IList where
-  ...
+  store :: Integer -> s -> s
+  load :: s -> Maybe (Integer, s)
 
 --
 gen :: IStore a => a
-
-gen = put 3 . put 2 . put 1 $ empty
+gen = store 3 . store 2 . store 1 $ empty
 
 --
-get3 :: IStore a => a -> Maybe [Integer]
+load3 :: IStore a => a -> Maybe [Integer]
 
-get3 s1 = ...
+load3 s1 = case load s1 of
+  Just (x, s2) -> case load s2 of
+    Just (y, s3) -> case load s3 of
+      Just (z, s4) -> Just [x,y,z]
+      Nothing -> Nothing
+    Nothing -> Nothing
+  Nothing -> Nothing
 
+load3' s1 = load s1 >>= \(x, s2) -> load s2 >>= \(y, s3) -> load s3 >>= \(z, s4) -> return [x,y,z]
 
+load3'' s1 = do
+  (x, s2) <- load s1
+  (y, s3) <- load s2
+  (z, s4) <- load s3
+  return [x,y,z]
+
+--
+newtype IList = NewList [Integer] deriving Show
+
+-- genIList :: IList
+genIList = foldr store (empty::IList) [1..10]
+
+instance IStore IList where
+  empty = NewList []
+  store x (NewList xs) = NewList (x:xs)
+  load (NewList []) = Nothing
+  load (NewList (x:xs)) = Just (x, NewList xs)
