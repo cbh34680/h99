9388a0bdfe232bba4042493a1faea8f92bbed695
cbh34680 <cbh34680@gmail.com>
2023-03-06 11:55:26 +0900

    test

--- splitOn.hs
+++ splitOn.hs
@@ -3,7 +3,15 @@ safeTail xs
   | null xs = []
   | otherwise = tail xs
 
+splitOn' :: Char -> String -> [String]
+
+splitOn' _ [] = []
+splitOn' sep cs = (takeWhile (/= sep) cs) : (splitOn sep . safeTail . dropWhile (/= sep) $ cs)
+
 splitOn :: Char -> String -> [String]
 
 splitOn _ [] = []
-splitOn sep cs = (takeWhile (/= sep) cs) : (splitOn sep . safeTail . dropWhile (/= sep) $ cs)
+splitOn sep cs = l : (splitOn sep . safeTail $ r)
+  where
+    (l,r) = break (== 'a') cs
+
